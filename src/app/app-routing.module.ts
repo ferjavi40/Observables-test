44a327123015f6c935b531ec0c440f12551539eb
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-02-18 16:34:49 +0100

    routes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //components
 import { HomeComponent } from './passengers/pages/home/home.component';
+import { PassengersComponent } from './passengers/pages/passengers/passengers.component';
+import { PasajeroComponent } from './passengers/pages/pasajero/pasajero.component';
+
+
 
 
 const routes: Routes = [
-  {path:'', component: HomeComponent, pathMatch: 'full'}
+  {path:'', component: HomeComponent, pathMatch: 'full'},
+  {path:'passengers', component: PassengersComponent},
+  {path: 'passenger/:id', component:PasajeroComponent},
+  {path: '**', redirectTo:''}
 ];
 
 @NgModule({
--- src/app/passengers/passengers.module.ts
+++ src/app/passengers/passengers.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//components
 import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PassengersComponent } from './pages/passengers/passengers.component';
+import { PasajeroComponent } from './pages/pasajero/pasajero.component';
 
 
 
 @NgModule({
   declarations: [
     CardComponent,
-    HomeComponent
+    HomeComponent,
+    PassengersComponent,
+    PasajeroComponent
   ],
   imports: [
     CommonModule

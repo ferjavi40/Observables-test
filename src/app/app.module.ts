bb736f6baf50c2d9912ce73d14b5914f08568b70
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-02-18 15:25:23 +0100

    maquetando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PassengersModule } from './passengers/passengers.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PassengersModule
   ],
   providers: [],
   bootstrap: [AppComponent]

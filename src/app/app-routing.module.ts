fa2cef8581217e9cb5a270f5be4811ba4ed51965
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-02-18 21:59:44 +0100

    before add footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { PasajeroComponent } from './passengers/pages/pasajero/pasajero.componen
 const routes: Routes = [
   {path:'', component: HomeComponent, pathMatch: 'full' },
   {path:'pasajeros', component: PassengersComponent},
-  {path: 'passenger/:id', component:PasajeroComponent},
+  {path: 'passenger', component:PasajeroComponent},
   {path: '**',  redirectTo:''}
 ];
 

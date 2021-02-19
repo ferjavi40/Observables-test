b1c5f649a4cf5d9e8e7f37e8e090c334ec31c408
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-02-19 16:49:52 +0100

    servicio firebase done

--- src/app/passengers/services/info-pagina.service.ts
+++ src/app/passengers/services/info-pagina.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 //interface
 import { DatosFjInterface } from '../interfaces/interfaces';
+import { PassengersInterface } from '../interfaces/passengers';
+
 
 
 
@@ -10,19 +12,33 @@ import { DatosFjInterface } from '../interfaces/interfaces';
 })
 export class InfoPaginaService {
 
-  info: DatosFjInterface  = {};
+  info: DatosFjInterface = {};
   cargada = false;
 
-  constructor(private http: HttpClient) {
+  passengers: PassengersInterface [] = [];
 
+
+  constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarPassengers();
+  }
+
+  private cargarInfo() {
+    //data from local 
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: DatosFjInterface) => {
         this.cargada = true;
-        this.info=resp;
-        console.log(resp);
+        this.info = resp;
       });
+  }
 
-
+  private cargarPassengers() {
+    //data from Firebase
+    this.http.get('https://test-observables-14aba-default-rtdb.europe-west1.firebasedatabase.app/passengers.json')
+        .subscribe((resp: PassengersInterface [] )=>{
+          this.passengers = resp;
+          console.log(resp);
+        });
   }
 
 
